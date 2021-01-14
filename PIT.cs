1da3b3181b7f9c70afa0e3a5ed0a925cd00a1bc1
Gorny1 <61839311+Gorny1@users.noreply.github.com>
2021-01-14 16:06:26 +0100

    Add files via upload
    hope it works lol

---
+++ PIT.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Pit
+{
+    double ubezpieczenie = 0.0775d;
+    public double podatekProgresywny(double kwota_podatku, double koszty, double zaliczki)
+    {
+        double wynik = 0d;
+        kwota_podatku = kwota_podatku - koszty;
+        kwota_podatku = kwota_podatku - ubezpieczenie;
+        if (kwota_podatku <= 85528d)
+        {
+            wynik = (kwota_podatku - (kwota_podatku * 0.17d)) - zaliczki;
+            wynik = kwota_podatku - wynik;
+            return wynik;
+        }
+        else
+        {
+            wynik = ((85528d * 0.17d) + ((kwota_podatku - 85528d) * 0.32d)) - zaliczki;
+            return wynik;
+        }
+
+    }
+
+    public double podatekLinowy(double kwota_podatku, double koszty, double zaliczki)
+    {
+        double wynik = 0d;
+        kwota_podatku = kwota_podatku - koszty;
+        kwota_podatku = kwota_podatku - ubezpieczenie;
+        wynik = (kwota_podatku - (kwota_podatku * 0.19d)) - zaliczki;
+        return wynik;
+    }
+
+    public double Ryczalt(double procent, double kwota_podatku, double koszty, double zaliczki)
+    {
+        double wynik = 0;
+        kwota_podatku = kwota_podatku - koszty;
+        kwota_podatku = kwota_podatku - ubezpieczenie;
+        wynik = (kwota_podatku - (kwota_podatku * (procent * (0.01))) - zaliczki);
+        return wynik;
+    }
+}
