785266d8f2470b36d11959efd5fcbb574b3a7a25
Patryk Kazimierowski <patrykkazimierowski@gmail.com>
2021-01-19 16:32:34 +0100

    create firs ver of main form

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,35 @@ namespace EconomicCalc
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_2(object sender, EventArgs e)
+        {
+
+        }
     }
 }
