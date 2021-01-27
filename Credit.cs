0d255b6c90a97268839f5d9f7ce0d020d25819a8
Joanna Kaźmierczak <asik7410@o2.pl>
2021-01-27 15:14:00 +0100

    creating a class

---
+++ Credit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorKredytowy
+{
+    class Credit
+    {
+        //obliczanie kwoty kredytu bez prowizji 
+        public double Installment(double AmountOfCredit, double Interest, double NumberOfInstallments)
+        {
+            double q = 1 + ((Interest / 100) / 12);
+            double LoanInstallment = AmountOfCredit * Math.Pow(q, NumberOfInstallments) * (q - 1) / (Math.Pow(q, NumberOfInstallments) - 1);
+            return Math.Round(LoanInstallment, 2);
+        }
+
+        //z prowizją od kwoty netto
+
+        public double InstallmentNetto(double AmountOfCredit, double Interest, double NumberOfInstallments, double Commission)
+        {
+            AmountOfCredit = AmountOfCredit * (1 + (Commission / 100));
+            double q = 1 + ((Interest / 100) / 12);
+            double LoanInstallment = AmountOfCredit * Math.Pow(q, NumberOfInstallments) * (q - 1) / (Math.Pow(q, NumberOfInstallments) - 1);
+            return Math.Round(LoanInstallment, 2);
+        }
+
+        //z prowizją do kwoty brutto
+        public double InstallmentBrutto(double AmountOfCredit, double Interest, double NumberOfInstallments, double Commission)
+        {
+            AmountOfCredit = AmountOfCredit / (1 - (Commission / 100));
+            double q = 1 + ((Interest / 100) / 12);
+            double LoanInstallment = AmountOfCredit * Math.Pow(q, NumberOfInstallments) * (q - 1) / (Math.Pow(q, NumberOfInstallments) - 1);
+            return Math.Round(LoanInstallment, 2);
+        }
+
+    }
+}
+
