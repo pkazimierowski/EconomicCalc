82707d0ddc29cc96c36c7679fb90f466e4e2e102
Patryk Kazimierowski <patrykkazimierowski@gmail.com>
2021-01-27 12:07:10 +0100

    add calsses exchange, rate roottobject jsonhandler

---
+++ Rate.cs
@@ -0,0 +1,12 @@
+﻿namespace EconomicCalc
+{
+    public class Rate
+    {
+        public string currency { get; set; }
+        public string code { get; set; }
+        public double bid { get; set; }
+        public double ask { get; set; }
+    }
+
+
+}
---
+++ RootObject.cs
@@ -0,0 +1,12 @@
+﻿
+using System.Collections.Generic;
+
+namespace EconomicCalc
+{
+    class RootObject
+    {
+        public string tradingDate { get; set; }
+        public string effectiveDate { get; set; }
+        public List<Rate> rates { get; set; }
+    }
+}
