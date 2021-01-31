7d20d59adc041bb80983900c6ca31a8ffbd6cb71
Gorny1 <61839311+Gorny1@users.noreply.github.com>
2021-01-31 10:48:30 +0100

    Add files via upload

--- Deposit.cs
+++ Deposit.cs
@@ -1,28 +1,39 @@
 ﻿namespace EconomicCalc
 {
-
+    /**
+ * Class Deposit is a simple class containing methos witch allows user to calculate
+ * their yearly income.
+ * static double lokataProgresywna caluclates progresive income
+ * static double lokataStatyczna calculates static income witch means that first money amount always increase by the same base value
+ *  Author - Dawid Górny
+ */
     static class Deposit
     {
-        public static double lokataStatyczna(int okres, double procent, double kwota)
+        public static double lokataStatyczna(int time_years, double procent, double money_amount)
         {
+            /**
+            * money_amount = takes the amount of inveted money
+            * procent = procentage increase;
+            * time_years = allows to calculate income over time, eq 10 years
+            */
             double wynik;
-            wynik = kwota;
+            wynik = money_amount;
             procent = procent * 0.01d;
-            for (int i = 0; i < okres; i++)
+            for (int i = 0; i < time_years; i++)
             {
-                wynik += kwota * procent;
+                wynik += money_amount * procent;
             }
             return wynik;
         }
 
-        public static double lokataProgresywna(int okres, double procent, double kwota)
+        public static double lokataProgresywna(int time_years, double procent, double money_amount)
         {
             procent = procent * 0.01d;
-            for (int i = 0; i < okres; i++)
+            for (int i = 0; i < time_years; i++)
             {
-                kwota += kwota * procent;
+                money_amount += money_amount * procent;
             }
-            return kwota;
+            return money_amount;
         }
     }
 }
